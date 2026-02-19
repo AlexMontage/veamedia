a7410acf746a7d563159250b2566cc93e84a4185
alex_montage <oleksandrchepiga2023@gmail.com>
2026-02-19 14:01:29 +0200

    Add files via upload

--- styles/burger-nav-scripts.js
+++ styles/burger-nav-scripts.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.getElementById('nav-menu');
     const navLinks = document.querySelectorAll('#nav-menu a');
 
-    // Функція перемикання
     const toggleMenu = () => {
         navMenu.classList.toggle('active');
         burgerBtn.classList.toggle('open');
@@ -14,7 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleMenu();
     });
 
-    // Закриваємо при кліку на посилання
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
             navMenu.classList.remove('active');
@@ -22,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Закриваємо, якщо клікнули десь поруч
+    // Закриття меню при кліку в будь-якому місці екрана
     document.addEventListener('click', (e) => {
         if (navMenu.classList.contains('active') && !navMenu.contains(e.target)) {
             navMenu.classList.remove('active');
