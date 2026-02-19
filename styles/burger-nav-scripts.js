2f7668b93956f2b7027d005da009b09450e9df06
alex_montage <oleksandrchepiga2023@gmail.com>
2026-02-19 14:04:38 +0200

    Add files via upload

--- styles/burger-nav-scripts.js
+++ styles/burger-nav-scripts.js
@@ -3,16 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.getElementById('nav-menu');
     const navLinks = document.querySelectorAll('#nav-menu a');
 
+    // Функція відкриття/закриття
     const toggleMenu = () => {
         navMenu.classList.toggle('active');
         burgerBtn.classList.toggle('open');
     };
 
+    // Клік по бургеру
     burgerBtn.addEventListener('click', (e) => {
         e.stopPropagation();
         toggleMenu();
     });
 
+    // Закриваємо меню при натисканні на будь-яке посилання
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
             navMenu.classList.remove('active');
@@ -20,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Закриття меню при кліку в будь-якому місці екрана
+    // Закриваємо меню при кліку в будь-якому місці екрана за межами меню
     document.addEventListener('click', (e) => {
         if (navMenu.classList.contains('active') && !navMenu.contains(e.target)) {
             navMenu.classList.remove('active');
